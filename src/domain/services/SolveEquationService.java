2c44e17d7dec0b56f46617446bdd57a7ca2f28ba
chico <chico.zimcolor@gmail.com>
2021-11-25 15:33:09 -0300

    implemented all four basic operations solver microsservices

--- src/domain/services/SolveEquationService.java
+++ src/domain/services/SolveEquationService.java
@@ -2,13 +2,19 @@ package domain.services;
 
 import domain.interfaces.SolverInterface;
 import domain.repositories.EquationElementRepository;
+import domain.services.microsservices.SolveDivideMicrosservice;
 import domain.services.microsservices.SolveFirstElementIfIsSubtractMicrosservice;
+import domain.services.microsservices.SolveMultiplyMicrosservice;
 import domain.services.microsservices.SolvePercentMicrosservice;
+import domain.services.microsservices.SolveSubtractMicrosservice;
 import domain.services.microsservices.SolveSumMicrosservice;
+import domain.valueObjects.Divide;
 import domain.valueObjects.EquationElement;
+import domain.valueObjects.Multiply;
 import domain.valueObjects.Number;
 import domain.valueObjects.Operation;
 import domain.valueObjects.Percent;
+import domain.valueObjects.Subtract;
 import domain.valueObjects.Sum;
 
 
@@ -17,6 +23,9 @@ public class SolveEquationService {
     private final SolvePercentMicrosservice solvePercentMicrosservice; 
     private final SolveFirstElementIfIsSubtractMicrosservice solveFirstElementIfIsSubtractMicrosservice;
     private final SolveSumMicrosservice solveSumMicrosservice;
+    private final SolveSubtractMicrosservice solveSubtractMicrosservice;
+    private final SolveMultiplyMicrosservice solveMultiplyMicrosservice;
+    private final SolveDivideMicrosservice solveDivideMicrosservice;
 
     
     public SolveEquationService(EquationElementRepository elementRepository) {
@@ -24,6 +33,9 @@ public class SolveEquationService {
         solvePercentMicrosservice = new SolvePercentMicrosservice(elementRepository);
         solveFirstElementIfIsSubtractMicrosservice = new SolveFirstElementIfIsSubtractMicrosservice(elementRepository);
         solveSumMicrosservice = new SolveSumMicrosservice(elementRepository);
+        solveSubtractMicrosservice = new SolveSubtractMicrosservice(elementRepository);
+        solveMultiplyMicrosservice = new SolveMultiplyMicrosservice(elementRepository);
+        solveDivideMicrosservice = new SolveDivideMicrosservice(elementRepository);
     }
     
     public void execute() {
@@ -36,7 +48,9 @@ public class SolveEquationService {
         solveFirstElementIfIsSubtractMicrosservice.execute();
         solveAllPercents();
         solveAllSums();
-        
+        solveAllSubtractions();
+        solveAllMultiplies();
+        solveAllDivisions();
     }
     
     private void solveAllPercents() {
@@ -45,6 +59,18 @@ public class SolveEquationService {
     
     private void solveAllSums() {
         solveRecursively(Sum.class, solveSumMicrosservice);
+    }
+    
+    private void solveAllSubtractions() {
+        solveRecursively(Subtract.class, solveSubtractMicrosservice);
+    }
+    
+    private void solveAllMultiplies() {
+        solveRecursively(Multiply.class, solveMultiplyMicrosservice);
+    }
+    
+    private void solveAllDivisions() {
+        solveRecursively(Divide.class, solveDivideMicrosservice);
     }
     
     private void solveRecursively(Class operationType, SolverInterface solverMicrosservice) {
---
+++ src/domain/services/microsservices/SolveSubtractMicrosservice.java
@@ -0,0 +1,33 @@
+package domain.services.microsservices;
+
+import domain.interfaces.SolverInterface;
+import domain.repositories.EquationElementRepository;
+import domain.valueObjects.Number;
+
+
+public class SolveSubtractMicrosservice implements SolverInterface {
+    private final EquationElementRepository elementRepository;
+
+    
+    public SolveSubtractMicrosservice(EquationElementRepository elementRepository) {
+        this.elementRepository = elementRepository;
+    }
+    
+    @Override
+    public void execute(int sumIndex) {
+        int firstNumberIndex = sumIndex - 1;
+        int lastNumberIndex = sumIndex + 1;
+       
+        Number firstNumber = (Number)elementRepository.getByIndex(firstNumberIndex);
+        Number lastNumber = (Number)elementRepository.getByIndex(lastNumberIndex);
+        
+        double newNumberValue = firstNumber.getValue() - lastNumber.getValue();
+        Number newNumber = new Number(newNumberValue);
+        
+        
+        elementRepository.removeIndex(lastNumberIndex);
+        elementRepository.removeIndex(sumIndex);
+        elementRepository.removeIndex(firstNumberIndex);
+        elementRepository.insert(firstNumberIndex, newNumber);
+    }
+}
