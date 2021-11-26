ea1e79460ea7877ac5b3091758e1aaadd7c633c6
chico <chico.zimcolor@gmail.com>
2021-11-26 12:05:06 -0300

    fixed operation solving order

--- src/domain/services/SolveEquationService.java
+++ src/domain/services/SolveEquationService.java
@@ -40,33 +40,23 @@ public class SolveEquationService {
         trimLooseOperationAtEnd.execute();
         solveFirstElementIfIsSubtractMicrosservice.execute();
         solveAllPercents();
-        solveAllMultiplies();
-        solveAllDivisions();
-        solveAllSums();
-        solveAllSubtractions();
+        solveMultipliesAndDivides();
+        solveSumsAndSubtractions();
     }
     
     private void solveAllPercents() {
-        solveRecursively(Percent.class, solvePercentMicrosservice);  
+        solveAllOfType(Percent.class, solvePercentMicrosservice);  
     }
     
-    private void solveAllSums() {
-        solveRecursively(Sum.class, solveOperationMicrosservice);
+    private void solveSumsAndSubtractions() {
+        solveAllOfType(Sum.class, Subtract.class, solveOperationMicrosservice);
     }
     
-    private void solveAllSubtractions() {
-        solveRecursively(Subtract.class, solveOperationMicrosservice);
+    private void solveMultipliesAndDivides() {
+        solveAllOfType(Multiply.class, Divide.class, solveOperationMicrosservice);
     }
     
-    private void solveAllMultiplies() {
-        solveRecursively(Multiply.class, solveOperationMicrosservice);
-    }
-    
-    private void solveAllDivisions() {
-        solveRecursively(Divide.class, solveOperationMicrosservice);
-    }
-    
-    private void solveRecursively(Class operationType, SolverInterface solverMicrosservice) {
+    private void solveAllOfType(Class operationType, SolverInterface solverMicrosservice) {
         int operationIndex = elementRepository.getIndexOfInstance(operationType);
         
         if (operationIndex < 0) {
@@ -75,6 +65,29 @@ public class SolveEquationService {
         
         solverMicrosservice.execute(operationIndex);
         
-        solveRecursively(operationType, solverMicrosservice);
+        solveAllOfType(operationType, solverMicrosservice);
+    }
+    
+    private void solveAllOfType(Class oneOperationType, Class otherOperationType, SolverInterface solverMicrosservice) {
+        int oneOperationIndex = elementRepository.getIndexOfInstance(oneOperationType);
+        int otherOperationIndex = elementRepository.getIndexOfInstance(otherOperationType);
+        int firstOperationIndex;
+        
+        if (oneOperationIndex < 0 && otherOperationIndex < 0) {
+            return;
+        }
+        else if (oneOperationIndex < 0) {
+            firstOperationIndex = otherOperationIndex;
+        }
+        else if (otherOperationIndex < 0) {
+            firstOperationIndex = oneOperationIndex;
+        }
+        else {
+            firstOperationIndex = Math.min(oneOperationIndex, otherOperationIndex);
+        }
+        
+        solverMicrosservice.execute(firstOperationIndex);
+        
+        solveAllOfType(oneOperationType, otherOperationType, solverMicrosservice);
     }
 }
